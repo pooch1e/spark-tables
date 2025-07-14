e5245f62f1ffc572a068d03f94b256bac21e5532
Joel Kram <joelnskram@gmail.com>
2025-07-14 14:26:54 +0100

    added some dev data

---
+++ data/development-data/table.js
@@ -0,0 +1,11 @@
+export const table = [
+  {
+    name: 'Wilderness',
+    description:
+      'The majority of any realm, where even rough trails are a rare sight',
+  },
+  {
+    name: 'Civilisation',
+    description: 'Rare havens of timber and stone, filled with joy and turmoil',
+  },
+];
