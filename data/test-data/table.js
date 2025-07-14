6f6faa5427d67fb2ea84df5ddfa4b8218de88868
Joel Kram <joelnskram@gmail.com>
2025-07-14 14:20:35 +0100

    moved test data to correct file

---
+++ data/test-data/table.js
@@ -0,0 +1,11 @@
+export const themes = [
+  {
+    name: 'Wilderness',
+    description:
+      'The majority of any realm, where even rough trails are a rare sight',
+  },
+  {
+    name: 'Civilisation',
+    description: 'Rare havens of timber and stone, filled with joy and turmoil',
+  },
+];
