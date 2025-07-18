35d64a4f92c47ebb75e3a2b7a0cf13f0d531018e
Joel Kram <joelnskram@gmail.com>
2025-07-18 09:55:47 +0100

    Remove initial database migration files and Prisma schema

--- __tests__/setup.test.ts
+++ __tests__/setup.test.ts
@@ -1,8 +1,3 @@
-import { prisma } from '../app/lib/prisma.ts';
-import { seed } from '../app/lib/seed.ts';
-import { cleanTestDatabase, resetTestSequences } from '../app/lib/prisma.ts';
-
-
 describe('testing database', () => {
   test('testing here', () => {
     console.log('im a test');
