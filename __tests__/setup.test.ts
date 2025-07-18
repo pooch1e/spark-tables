3e56087ad091a3f3e0dde0017c33ae31089b69fd
Joel Kram <joelnskram@gmail.com>
2025-07-18 14:54:42 +0100

    writing tests for topics table

--- __tests__/setup.test.ts
+++ __tests__/setup.test.ts
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: '.env.test' });
+import { eq } from 'drizzle-orm';
+import { sql } from 'drizzle-orm';
 
 import { closeConnection, db } from '@/db/connections.ts';
 import { seed } from '@/db/seeds/seed.ts';
@@ -14,6 +16,7 @@ import {
 console.log('Connected to DB:', process.env.TEST_DATABASE_URL);
 beforeAll(async () => {
   await db.delete(topics);
+  await db.execute(sql`TRUNCATE TABLE topics RESTART IDENTITY CASCADE`);
   await seed(topicsTestData);
 });
 afterAll(async () => {
@@ -25,5 +28,9 @@ describe('testing database', () => {
   test('table of topics exists', async () => {
     const allTopics = await db.select().from(topics);
     expect(allTopics.length).toBeGreaterThan(0);
+  });
+  test('topics has id of of a number', async () => {
+    const [topic] = await db.select().from(topics).where(eq(topics.id, 1));
+    expect(topic.id).toBe(1);
   });
 });
