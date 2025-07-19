b1ac31fdefa25db1f577da8de918278a627103ce
Joel Kram <joelnskram@gmail.com>
2025-07-19 13:27:03 +0100

    add: tests for api/topics

---
+++ __tests__/endpoints.test.ts
@@ -0,0 +1,8 @@
+describe('testing endpoints', () => {
+  describe('GET: api/topics', () => {
+    test.todo('200: returns empty array when no topics exist');
+    test.todo('200: returns array of all topics');
+    test.todo('200: topics are returned with correct properties');
+    test.todo('500: handles database connection errors');
+  });
+});
