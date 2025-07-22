e02da0f161a06f88fff8513bcf02a1390915fa5c
Joel Kram <joelnskram@gmail.com>
2025-07-22 11:09:48 +0100

    removed id from dev data

--- data/development-data/themes.ts
+++ data/development-data/themes.ts
@@ -1,88 +1,74 @@
 export const themes = [
   // Wilderness themes
   {
-    id: 1,
     name: 'Land',
     order: 1,
     topic_id: 1,
   },
   {
-    id: 2,
     name: 'Sky',
     order: 2,
     topic_id: 1,
   },
   {
-    id: 3,
     name: 'Flora',
     order: 3,
     topic_id: 1,
   },
   {
-    id: 4,
     name: 'Fauna',
     order: 4,
     topic_id: 1,
   },
   // Civilisation themes
   {
-    id: 5,
     name: 'People',
     order: 1,
     topic_id: 2,
   },
   {
-    id: 6,
     name: 'Streets',
     order: 2,
     topic_id: 2,
   },
   {
-    id: 7,
     name: 'Buildings',
     order: 3,
     topic_id: 2,
   },
   {
-    id: 8,
     name: 'Commerce',
     order: 4,
     topic_id: 2,
   },
   // Dungeons themes
   {
-    id: 9,
     name: 'Architecture',
     order: 1,
     topic_id: 3,
   },
   {
-    id: 10,
     name: 'Atmosphere',
     order: 2,
     topic_id: 3,
   },
   {
-    id: 11,
     name: 'Hazards',
     order: 3,
     topic_id: 3,
   },
   // Maritime themes
   {
-    id: 12,
     name: 'Waters',
     order: 1,
     topic_id: 4,
   },
   {
-    id: 13,
     name: 'Vessels',
     order: 2,
     topic_id: 4,
   },
   {
-    id: 14,
     name: 'Coastal',
     order: 3,
     topic_id: 4,
