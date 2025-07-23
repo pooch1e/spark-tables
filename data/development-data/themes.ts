a6b2a6f840236d30fa11f5e14688fff5f3579c40
Joel Kram <joelnskram@gmail.com>
2025-07-23 16:41:34 +0100

    rmv invalid data from dev data

--- data/development-data/themes.ts
+++ data/development-data/themes.ts
@@ -92,36 +92,4 @@ export const themes = [
     order: 9,
     topic_id: 2,
   },
-  // Dungeons themes
-  {
-    name: 'Architecture',
-    order: 1,
-    topic_id: 3,
-  },
-  {
-    name: 'Atmosphere',
-    order: 2,
-    topic_id: 3,
-  },
-  {
-    name: 'Hazards',
-    order: 3,
-    topic_id: 3,
-  },
-  // Maritime themes
-  {
-    name: 'Waters',
-    order: 1,
-    topic_id: 4,
-  },
-  {
-    name: 'Vessels',
-    order: 2,
-    topic_id: 4,
-  },
-  {
-    name: 'Coastal',
-    order: 3,
-    topic_id: 4,
-  },
 ];
