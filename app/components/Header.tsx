729b543b44a768f08258d9f8587c272c11ca4366
Joel Kram <joelnskram@gmail.com>
2025-07-25 09:58:37 +0100

    add header comp

---
+++ app/components/Header.tsx
@@ -0,0 +1,11 @@
+'use client';
+export default function Header() {
+  return (
+    <>
+      {' '}
+      <h1>Spark Forge</h1>
+      <h2>Inspiration Tables</h2>
+      <p>sticky header accross site</p>
+    </>
+  );
+}
