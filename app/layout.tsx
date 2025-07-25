e24e1e5bf1815fe9d42ad10196516341662d841f
Joel Kram <joelnskram@gmail.com>
2025-07-25 14:15:32 +0100

    add: title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Electrolize, Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Header from './components/Header';
 
@@ -11,6 +11,12 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
+});
+
+const electrolize = Electrolize({
+  variable: '--font-electrolize',
+  subsets: ['latin'],
+  weight: '400',
 });
 
 export const metadata: Metadata = {
@@ -26,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        className={`${geistSans.variable} ${geistMono.variable} ${electrolize.variable} antialiased`}>
         <Header />
         {children}
       </body>
