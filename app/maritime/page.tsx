1066d7b041750d9405bdd072d928278f65e61842
Joel Kram <joelnskram@gmail.com>
2025-07-29 11:24:49 +0100

    completed: dev data

--- app/maritime/page.tsx
+++ app/maritime/page.tsx
@@ -1,3 +1,11 @@
-export default function MaritimePage() {
-  return <p>maritime page</p>;
+import { TopicService } from '../lib/services/topicService';
+import SparkTable from '../components/SparkTableComp/SparkTable';
+export default async function MaritimePage() {
+  const maritimeData = await TopicService.getAllTopicsDataById(4);
+  // console.log(dungeonData);
+  return (
+    <div>
+      <SparkTable data={maritimeData} />
+    </div>
+  );
 }
