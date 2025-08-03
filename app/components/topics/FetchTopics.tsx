0bf278dc6ffcf6c0d135550b8f9852feae2c37d9
Joel Kram <joelnskram@gmail.com>
2025-08-03 13:36:14 +0100

    update: styling on topics page

--- app/components/topics/FetchTopics.tsx
+++ app/components/topics/FetchTopics.tsx
@@ -8,9 +8,15 @@ export default async function TopicsTable() {
     <div className="space-y-2">
       {topics.map((topic) => (
         <div key={topic.id}>
-          <div className="grid grid-cols-2 gap-4 border-1 p-2 text-center">
-            <Link href={`/${topic.name.toLowerCase()}`}>{topic.name}</Link>
-            <p>{topic.description}</p>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 w-full p-2">
+            <Link
+              href={`/${topic.name.toLowerCase()}`}
+              className="grid grid-cols-2 gap-4 border-1 p-2">
+              {topic.name}
+            </Link>
+            <p className="grid grid-cols-2 gap-4 border-1 p-2">
+              {topic.description}
+            </p>
           </div>
         </div>
       ))}
